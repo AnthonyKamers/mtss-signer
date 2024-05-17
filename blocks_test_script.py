e0fcffe8c2d25d27286c859056757e4845ef900a
Anthony Kamers <anthonybernardo10@gmail.com>
2024-05-17 09:52:24 -0300

    Comparação de tempo para criação de CFFs (Sperner-Polynomial) 	* Altera nomes dos arquivos para facilitar o testes

--- blocks_test_script.py
+++ blocks_test_script.py
@@ -9,9 +9,27 @@ from mtsssigner.utils.file_and_block_utils import *
 
 # print(correta == errada)
 
+TEXT_PATH = "msg/correct/hash/"
+TEXT_EXTENSION = ".txt"
+
+XML_PATH = "msg/xml/"
+XML_EXTENSION = ".xml"
+
+files = ["64", "512", "4096", "32768"]
+
 if __name__ == '__main__':
-    file_path = sys.argv[1]
-    start = timer()
-    get_message_and_blocks_from_file(file_path)
-    end = timer()
-    print(end - start)
+    for file in files:
+        file_path = TEXT_PATH + file + TEXT_EXTENSION
+
+        start_text = timer()
+        get_message_and_blocks_from_file(file_path)
+        end_text = timer()
+        diff_text = end_text - start_text
+
+        xml_path = XML_PATH + file + XML_EXTENSION
+        start_xml = timer()
+        get_message_and_blocks_from_file(xml_path)
+        end_xml = timer()
+        diff_xml = end_xml - start_xml
+
+        print(diff_text, diff_xml, diff_xml - diff_text)
