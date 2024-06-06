bfb1fd8a399cff3151f052a8ade74e29c4ff713f
Anthony Kamers <anthonybernardo10@gmail.com>
2024-06-06 17:11:08 -0300

    Adiciona mais arquivos para teste de divisão de documento em blocos

--- blocks_test_script.py
+++ blocks_test_script.py
@@ -1,13 +1,8 @@
-import sys
 from timeit import default_timer as timer
 
+import matplotlib.pyplot as plt
+
 from mtsssigner.utils.file_and_block_utils import *
-
-# numpy.set_printoptions(threshold=sys.maxsize)
-# correta = create_polynomial_cff(3,2)
-# errada = create_polynomial_cff_2(3,2)
-
-# print(correta == errada)
 
 TEXT_PATH = "msg/correct/hash/"
 TEXT_EXTENSION = ".txt"
@@ -15,21 +10,55 @@ TEXT_EXTENSION = ".txt"
 XML_PATH = "msg/xml/"
 XML_EXTENSION = ".xml"
 
-files = ["64", "512", "4096", "32768"]
+files = ["64", "256", "512", "1024", "4096", "8192", "16384", "32768"]
 
-if __name__ == '__main__':
+
+# https://stackoverflow.com/questions/41383787/round-down-to-2-decimal-in-python
+def round_down(value, decimals):
+    factor = 1 / (10 ** decimals)
+    return (value // factor) * factor
+
+
+def main():
+    values_txt = []
+    values_xml = []
+
     for file in files:
         file_path = TEXT_PATH + file + TEXT_EXTENSION
 
         start_text = timer()
         get_message_and_blocks_from_file(file_path)
         end_text = timer()
-        diff_text = end_text - start_text
+        diff_text = round_down((end_text - start_text) * 1000, 4)
+        values_txt.append(diff_text)
 
         xml_path = XML_PATH + file + XML_EXTENSION
         start_xml = timer()
         get_message_and_blocks_from_file(xml_path)
         end_xml = timer()
-        diff_xml = end_xml - start_xml
+        diff_xml = round_down((end_xml - start_xml) * 1000, 4)
+        values_xml.append(diff_xml)
 
-        print(diff_text, diff_xml, diff_xml - diff_text)
+    generate_graph(values_txt, values_xml)
+
+
+def generate_graph(data_txt, data_xml):
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+
+    ax.plot(files, data_txt, 'g', label="TXT")
+    ax.plot(files, data_xml, 'r', label="XML")
+    ax.legend()
+
+    ax.set_yscale('log')
+
+    ax.set_ylabel('Time (ms)')
+    ax.set_xlabel('Number of blocks n')
+    ax.set_title("DivideBlocks algorithm efficiency")
+
+    # plt.show()
+    plt.savefig("divide_blocks_efficiency.png", bbox_inches='tight')
+
+
+if __name__ == '__main__':
+    main()
