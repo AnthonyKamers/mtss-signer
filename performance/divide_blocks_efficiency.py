2a30b045025d76373f2cd38a78b922faff8f7601
Anthony Kamers <anthonybernardo10@gmail.com>
2024-12-18 15:07:33 -0300

    performance de DivideBlocks em formato para gráfico + atualização de imagens (para serem maiores e conterem mais blocos)

--- performance/divide_blocks_efficiency.py
+++ performance/divide_blocks_efficiency.py
@@ -1,3 +1,4 @@
+import pprint
 from timeit import default_timer as timer
 from typing import Tuple
 
@@ -8,7 +9,7 @@ from mtsssigner.blocks.CSVParser import DELIMITER, CSVParser
 from mtsssigner.blocks.ImageParser import ImageParser
 from mtsssigner.blocks.block_utils import get_parser_for_file
 
-ITERATION = 100
+ITERATION = 1
 OUTPUT_FILE = 'performance/output/divide_blocks_efficiency.txt'
 FOLDER_RELATIVE_ROOT = 'msg/bytes'
 order = ['json', 'txt', 'csv', 'xml', 'pdf', 'png']
@@ -18,17 +19,26 @@ CSV_DELIMITER = DELIMITER.BREAK_LINE
 
 
 def generate_output(output: dict[str, Tuple[float, int]]) -> None:
+    string = f'bytes json json_n txt txt_n csv csv_n xml xml_n pdf pdf_n png png_n\n'
+
+    for size in sizes:
+        string += f'{size}'
+        for extension in order:
+            filename = f'{size}.{extension}'
+            time, amount_blocks = output[filename]
+
+            string += f' {time} {amount_blocks}'
+        string += '\n'
+
     with open(OUTPUT_FILE, 'w') as file:
-        for key, value in output.items():
-            time, amount_blocks = value
-            file.write(f'{key} {amount_blocks} {time}\n')
+        file.write(string)
 
 
 def main():
     output = {}
     for iteration in range(ITERATION):
-        for size in sizes:
-            for extension in order:
+        for extension in order:
+            for size in sizes:
                 filename = f'{size}.{extension}'
                 filepath = f'{FOLDER_RELATIVE_ROOT}/{extension}/{filename}'
 
@@ -53,7 +63,8 @@ def main():
                     output[filename] = (to_ms(start, end), amount_blocks)
                 else:
                     # if it is not the first iteration, take the average between the previous and the current
-                    output[filename] = ((output[filename] + to_ms(start, end)) / 2, amount_blocks)
+                    output[filename] = ((output[filename][0] + to_ms(start, end)) / 2, amount_blocks)
+        print(f'Iteration {iteration + 1} finished')
 
     generate_output(output)
 
