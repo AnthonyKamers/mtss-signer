fcf81ac4fef667845936d14c6dc772adf5d81f14
Anthony Kamers <anthonybernardo10@gmail.com>
2024-12-26 21:21:45 -0300

    Gerando CFF polinomial passando apenas d, n

--- cffs/generate_cff.py
+++ cffs/generate_cff.py
@@ -1,8 +1,10 @@
 import typer
 import sys
 
-sys.path.append('.')
 
+sys.path.append('../')
+
+from mtsssigner.cffs.cff_utils import get_parameters_polynomial_cff, parse_file
 from mtsssigner.cff_builder import create_1_cff, create_cff
 from mtsssigner.utils.file_and_block_utils import write_cff_to_file
 
@@ -15,6 +17,20 @@ def sperner(n: int):
     t = len(cff)
 
     write_cff_to_file(t, n, 1, cff)
+
+
+@app.command()
+def polynomial_d(d: int, n: int):
+    parse_file()
+
+    q, k, n_expected, t = get_parameters_polynomial_cff(d, n)
+    n = n_expected
+
+    print(f'q: {q}, k: {k}, n: {n}, t: {t}')
+
+    cff = create_cff(q, k)
+
+    write_cff_to_file(t, n, d, cff)
 
 
 @app.command()
